a864ab8d6a25ae44054cdf7a88bd874db2b26569
GiovaneAnjos <giovane.anjos@edu.unipar.br>
2025-04-10 11:41:24 -0300

    docs: Conclusão aulas Orientação Objetos - Associação pt. 05

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Professor.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Professor.java
@@ -2,9 +2,54 @@ package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
 
 public class Professor {
     private String nome;
+    private String especialidade;
+    private Seminario[] seminarios;
 
     public Professor(String nome) {
         this.nome = nome;
+    }
+
+    public Professor(String nome, String especialidade) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+    }
+
+    public Professor(String nome, String especialidade, Seminario[] seminarios) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+        this.seminarios = seminarios;
+    }
+
+    public void imprime() {
+        System.out.println("--------------------------");
+        System.out.println("Professor: " + this.nome);
+        if (this.seminarios == null) return;
+        System.out.println("## Seminários cadastrados ##");
+        for (Seminario seminario : this.seminarios) {
+            System.out.println("Seminário: "+seminario.getTitulo());
+            System.out.println("Local: "+seminario.getLocal().getEndereco());
+            if (seminario.getAluno() == null || seminario.getAluno().length == 0) continue;
+            System.out.println("** Alunos **");
+            for (Aluno aluno : seminario.getAluno()) {
+                System.out.println("Aluno: " + aluno.getNome() + " idade: " + aluno.getIdade());
+            }
+        }
+    }
+
+    public Seminario[] getSeminarios() {
+        return seminarios;
+    }
+
+    public void setSeminarios(Seminario[] seminarios) {
+        this.seminarios = seminarios;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
     }
 
     public String getNome() {
